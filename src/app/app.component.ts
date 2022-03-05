3b1e58ac393903bc98ab22456ee8452113686a2f
Arc1llusion <arc1llusion@gmail.com>
2022-03-04 23:24:00 -0500

    Chart of per coin over time

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,10 +24,27 @@ class DataSeries {
 export class AppComponent implements OnInit {
     title = 'CoinCounting-App';
 
-    public data: Data[] = [];
+    public coins: CoinDto[] = [];
+    public totalData: Data[] = [];
+    public coinData: Data[] = [
+        {
+            name: "Pennies",
+            series: []
+        },
+        {
+            name: "Nickels",
+            series: []
+        },
+        {
+            name: "Dimes",
+            series: []
+        },
+        {
+            name: "Quarters",
+            series: []
+        },
+    ]
     public total: number = 0;
-
-    public updateChartSubject: Subject<any> = new Subject();
 
     constructor(
         private api: CoinApi,
@@ -36,10 +53,10 @@ export class AppComponent implements OnInit {
 
     async ngOnInit(): Promise<void> {
         this.signalR.StartConnection();
-        let coins = await this.api.ListDeposits();
+        this.coins = await this.api.ListDeposits();
         let category: Data[] = [{ name: "Coins", series: [] }];
 
-        let series = coins.map((coin) => {
+        let series = this.coins.map((coin) => {
             let sum = coin.Pennies * .01 + coin.Nickels * .05 + coin.Dimes * 0.10 + coin.Quarters * 0.25;
             return {
                 name: new Date(coin.DateDeposited),
@@ -48,29 +65,64 @@ export class AppComponent implements OnInit {
         });
 
         category[0].series = series;
-        this.data = [...category];
+        this.totalData = [...category];
 
         this.CalculateTotal();
+        this.TransformIndividualCoinData(this.coins);
 
         this.signalR.DepositBroadcast.subscribe((coinDto: CoinDto) => {
-            console.log(typeof coinDto.Pennies);
             let sum = coinDto.Pennies * .01 + coinDto.Nickels * .05 + coinDto.Dimes * 0.10 + coinDto.Quarters * 0.25;
-            this.data[0].series!.push({
+            this.totalData[0].series!.push({
                 name: new Date(coinDto.DateDeposited),
                 value: parseFloat(sum.toFixed(2))
             });
+
             this.zone.run(() => {
-                this.data = [...this.data];
+                this.totalData = [...this.totalData];
                 this.CalculateTotal();
+
+                this.coins.push(coinDto);
+                this.TransformIndividualCoinData(this.coins);
             });
         });
     }
 
     public CalculateTotal() {
-        let series = this.data[0].series;
+        let series = this.totalData[0].series;
 
         this.total = Enumerable.from(series!).sum((x) => {
             return x.value;
         });
+
+        this.total = parseFloat(this.total.toFixed(2));
+    }
+
+    public TransformIndividualCoinData(coins: CoinDto[]) {
+        let pennies = this.coinData[0];
+        let nickels = this.coinData[1];
+        let dimes = this.coinData[2];
+        let quarters = this.coinData[3];
+
+        pennies.series = coins.map((c) => <DataSeries>{
+            name: c.DateDeposited,
+            value: c.Pennies
+        });
+
+        nickels.series = coins.map((c) => <DataSeries>{
+            name: c.DateDeposited,
+            value: c.Nickels
+        });
+
+        dimes.series = coins.map((c) => <DataSeries>{
+            name: c.DateDeposited,
+            value: c.Dimes
+        });
+
+        quarters.series = coins.map((c) => <DataSeries>{
+            name: c.DateDeposited,
+            value: c.Quarters
+        });
+
+        this.coinData = [...this.coinData];
     }
 }
